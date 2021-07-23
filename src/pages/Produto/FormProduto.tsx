3bab5aa430a1739a8cf1785e7a2a7c52959721b9
Jodeilson Weslley <jodeilson.weslley@logiquesistemas.com.br>
2021-07-23 01:15:30 -0300

    Acrescento de novos dados no form de produto

--- src/pages/Produto/FormProduto.tsx
+++ src/pages/Produto/FormProduto.tsx
@@ -17,6 +17,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { Button } from 'react-native-ui-lib';
 import api from '../../api/api';
 import { Picker } from '@react-native-picker/picker';
+import { MaskedTextInput } from 'react-native-mask-text';
 
 const plusIcon = require('../../../assets/icons/plusSmall.png');
 const image = require('../../../assets/frutas.jpg');
@@ -39,20 +40,43 @@ export function FormProduto() {
 	const [name3, setName3] = useState<string>();
 	const [name3Error, setName3Error] = useState<string>('');
 
+	const [isFocus4, setIsFocus4] = useState(false);
+	const [isFilled4, setIsFilled4] = useState(false);
+	const [name4, setName4] = useState<string>('0,00');
+	const [name4Error, setName4Error] = useState<string>('');
+
 	const [date, setDate] = useState('2021-08-22');
 	const [submitError, setSubmitError] = useState<string>('');
 
-	const [categoria, setCategoria] = useState();
+	const [categoria, setCategoria] = useState<string>('fruta');
+	const [unidadeMedida, setUnidadeMedida] = useState<string>('quilo');
 
 	async function handleSubmit() {
-		if (name1Error == '' && name2Error == '' && name3Error == '') {
+		console.log('nome:', name1);
+		console.log('categoria:', categoria);
+		console.log('quantidade:', name3);
+		console.log('unidadeMedida:', unidadeMedida);
+		console.log('preco:', name4.split('$')[1]);
+
+		if (name1Error == '' && name3Error == '' && name4Error == '' && categoria != '' && unidadeMedida != '') {
 			setSubmitError('');
-			await api.post('/produto', {
-				nome: name1,
-				categoria: name2,
-				qtdCaixa: name3,
-				dataValidade: Date.parse(date),
-			});
+			await api
+				.post('/produto', {
+					nome: name1,
+					categoria: categoria,
+					quantidade: name3,
+					unidadeMedida: unidadeMedida,
+					preco: name4.split('$')[1],
+					dataValidade: Date.parse(date),
+				})
+				.then((res) => {
+					console.log(res);
+
+					goToListProduto();
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 
 			navigation.navigate('ListProduto');
 		} else {
@@ -120,6 +144,25 @@ export function FormProduto() {
 		setName3(value);
 	}
 
+	function handleFocus4() {
+		setIsFocus4(true);
+	}
+
+	function handleBlur4() {
+		setIsFocus4(false);
+
+		if (name3 == '' || name3 == null || name3 == undefined) {
+			setName4Error('Campo obrigatório');
+		} else {
+			setName4Error('');
+		}
+	}
+
+	function handleInputChange4(value: string) {
+		setIsFilled4(!!value);
+		setName4(value);
+	}
+
 	return (
 		<ImageBackground source={image} resizeMode='cover' style={styles.image}>
 			<SafeAreaView style={styles.container}>
@@ -136,54 +179,109 @@ export function FormProduto() {
 									onChangeText={handleInputChange1}
 								></TextInput>
 								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name1Error}</Text>
-								<Text style={styles.inputHint}>Categoria do produto</Text>
-								<Picker
-									style={styles.select}
-									selectedValue={categoria}
-									onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}
-								>
-									<Picker.Item label='Fruta' value='fruta' />
-									<Picker.Item label='Grão' value='grao' />
-									<Picker.Item label='Verdura' value='verdura' />
-									<Picker.Item label='Legume' value='legume' />
-									<Picker.Item label='Hortaliça' value='hortaliça' />
-								</Picker>
-								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name2Error}</Text>
-								<TextInput
-									style={[styles.input, (isFocus2 || isFilled2) && { borderColor: '#5cd65c' }]}
-									placeholder='Caixas disponíveis'
-									onFocus={handleFocus3}
-									onBlur={handleBlur3}
-									onChangeText={handleInputChange3}
-									keyboardType='numeric'
-								></TextInput>
-								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name3Error}</Text>
-								<DatePicker
-									style={styles.datePickerStyle}
-									date={date}
-									mode='date'
-									placeholder='Data de validade'
-									format='YYYY-MM-DD'
-									confirmBtnText='Confirm'
-									cancelBtnText='Cancel'
-									customStyles={{
-										dateIcon: {
-											position: 'absolute',
-											left: 0,
-											top: 4,
-											marginLeft: 0,
-										},
-										dateInput: {
-											marginLeft: 36,
-										},
-									}}
-									onDateChange={(date) => {
-										setDate(date);
-									}}
-								/>
-								<View style={{ alignItems: 'center', justifyContent: 'space-around' }}>
-									{/* <Button  title='Cadastrar' activeOpacity={0.7} onPress={handleSubmit} /> */}
+								<View style={styles.container}>
+									<View style={styles.row}>
+										<Text style={styles.inputHint}>Categoria do produto: </Text>
+										<Picker
+											style={styles.select}
+											selectedValue={categoria}
+											onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}
+										>
+											<Picker.Item label='Fruta' value='fruta' />
+											<Picker.Item label='Grão' value='grao' />
+											<Picker.Item label='Verdura' value='verdura' />
+											<Picker.Item label='Legume' value='legume' />
+											<Picker.Item label='Hortaliça' value='hortaliça' />
+										</Picker>
+									</View>
+								</View>
+								<View style={styles.container}>
+									<View style={styles.row}>
+										<Text style={styles.inputHint}>Unidade de medida: </Text>
+										<Picker
+											style={styles.select}
+											selectedValue={unidadeMedida}
+											onValueChange={(itemValue, itemIndex) => {
+												setUnidadeMedida(itemValue);
+												console.log('i-', itemValue);
+												console.log(unidadeMedida);
+											}}
+										>
+											<Picker.Item label='Quilo' value='quilo' />
+											<Picker.Item label='Unidade' value='unidade' />
+											<Picker.Item label='Arroba' value='arroba' />
+											<Picker.Item label='Tonelada' value='tonelada' />
+										</Picker>
+									</View>
+								</View>
+								<View style={styles.container}>
+									<View style={styles.row}>
+										<Text style={styles.inputHint}>Quantidade: </Text>
+										<TextInput
+											style={[(isFocus3 || isFilled3) && { borderColor: '#5cd65c' }]}
+											placeholder='Quantidade'
+											onFocus={handleFocus3}
+											onBlur={handleBlur3}
+											onChangeText={handleInputChange3}
+											keyboardType='numeric'
+										></TextInput>
+										<Text style={(styles.fullWidth, { color: 'tomato', alignSelf: 'center' })}>
+											{name3Error}
+										</Text>
+									</View>
+								</View>
+								<View style={styles.viewPreco}>
+									<Text style={styles.inputHint2}>Preço por medida</Text>
+									<MaskedTextInput
+										type='currency'
+										options={{
+											prefix: 'R$',
+											decimalSeparator: ',',
+											groupSeparator: '.',
+											precision: 2,
+										}}
+										style={[
+											styles.inputPreco,
+											(isFocus4 || isFilled4) && { borderColor: '#5cd65c' },
+										]}
+										placeholder='Preço'
+										onFocus={handleFocus4}
+										onBlur={handleBlur4}
+										onChangeText={handleInputChange4}
+										keyboardType='numeric'
+									/>
+									<Text style={(styles.fullWidth, { color: 'tomato', alignSelf: 'center' })}>
+										{name3Error}
+									</Text>
+								</View>
+								<View style={styles.viewPreco}>
+									<Text style={styles.inputHint2}>Validade</Text>
+									<DatePicker
+										style={styles.datePickerStyle}
+										date={date}
+										mode='date'
+										placeholder='Data de validade'
+										format='YYYY-MM-DD'
+										confirmBtnText='Confirm'
+										cancelBtnText='Cancel'
+										customStyles={{
+											dateIcon: {
+												position: 'absolute',
+												left: 0,
+												top: 4,
+												marginLeft: 0,
+											},
+											dateInput: {
+												marginLeft: 36,
+											},
+										}}
+										onDateChange={(date) => {
+											setDate(date);
+										}}
+									/>
+								</View>
 
+								<View style={{ alignItems: 'center', justifyContent: 'space-around' }}>
 									<Button
 										backgroundColor='#30B650'
 										label='Cadastrar'
@@ -195,7 +293,7 @@ export function FormProduto() {
 										onPress={handleSubmit}
 									/>
 									<Text style={{ color: 'tomato', alignSelf: 'center' }}>{submitError}</Text>
-									{/* <Button title='Listar produtos' onPress={goToListProduto} /> */}
+									<Button label='Listar produtos' onPress={goToListProduto} />
 								</View>
 							</ScrollView>
 						</View>
@@ -213,12 +311,19 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		marginTop: 80,
 		color: '#598234',
-		fontFamily: 'Roboto-Black',
 	},
 
 	image: {
 		flex: 1,
 		justifyContent: 'center',
+	},
+
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		width: '90%',
+		backgroundColor: '#dbdbdb',
+		flexWrap: 'wrap',
 	},
 
 	container: {
@@ -235,13 +340,12 @@ const styles = StyleSheet.create({
 
 	input: {
 		fontSize: 18,
-		marginTop: 50,
+		marginTop: 20,
 		padding: 10,
 		borderBottomWidth: 1,
 		color: '#598234',
 		width: '100%',
 		textAlign: 'center',
-		fontFamily: 'Roboto-Black',
 	},
 
 	link: {
@@ -251,21 +355,48 @@ const styles = StyleSheet.create({
 
 	inputHint: {
 		fontSize: 18,
-		marginTop: 10,
-		padding: 10,
 		color: 'black',
+		backgroundColor: '#dbdbdb',
+		padding: 10,
+		width: '50%',
+	},
+
+	inputPreco: {
+		width: '50%',
+		fontSize: 18,
+		marginTop: 20,
+		padding: 10,
+		borderBottomWidth: 1,
+		color: 'black',
+	},
+
+	fullWidth: {
 		width: '100%',
-		textAlign: 'center',
+	},
+
+	viewPreco: {
+		flex: 1,
+		flexDirection: 'row',
 	},
 
 	select: {
 		fontSize: 15,
-		color: '#598234',
-		width: '100%',
+		color: '#000000',
+		width: '50%',
+		backgroundColor: '#dbdbdb',
+		padding: 10,
+	},
+
+	inputHint2: {
+		fontSize: 20,
+		color: 'black',
+		padding: 10,
+		width: '50%',
+		marginStart: 10,
+		marginTop: 20,
 	},
 
 	datePickerStyle: {
-		width: 200,
 		marginTop: 20,
 	},
 });
