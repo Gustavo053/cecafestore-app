e1818253fe1acec345eaab58f7bd2828dd11701b
Jodeilson Weslley <jodeilson.weslley@logiquesistemas.com.br>
2021-07-22 21:52:01 -0300

    Adição de assets para trabalhar posteriormente com a interface

--- src/pages/Produto/FormProduto.tsx
+++ src/pages/Produto/FormProduto.tsx
@@ -1,21 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import {
 	SafeAreaView,
 	KeyboardAvoidingView,
 	Keyboard,
 	Platform,
 	TouchableWithoutFeedback,
-	TouchableOpacity,
 	StyleSheet,
 	View,
 	TextInput,
 	Text,
+	ImageBackground,
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
-
-import { Button } from '../../components/Button';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button } from 'react-native-ui-lib';
 import api from '../../api/api';
+import { Picker } from '@react-native-picker/picker';
+
+const plusIcon = require('../../../assets/icons/plusSmall.png');
+const image = require('../../../assets/frutas.jpg');
 
 export function FormProduto() {
 	const navigation = useNavigation();
@@ -37,6 +41,8 @@ export function FormProduto() {
 
 	const [date, setDate] = useState('2021-08-22');
 	const [submitError, setSubmitError] = useState<string>('');
+
+	const [categoria, setCategoria] = useState();
 
 	async function handleSubmit() {
 		if (name1Error == '' && name2Error == '' && name3Error == '') {
@@ -115,77 +121,104 @@ export function FormProduto() {
 	}
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<View>
-						<Text style={styles.title}>Cadastro de produto</Text>
-						<TextInput
-							style={[styles.input, (isFocus1 || isFilled1) && { borderColor: '#5cd65c' }]}
-							placeholder='Nome do produto'
-							onFocus={handleFocus1}
-							onBlur={handleBlur1}
-							onChangeText={handleInputChange1}
-						></TextInput>
-						<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name1Error}</Text>
+		<ImageBackground source={image} resizeMode='cover' style={styles.image}>
+			<SafeAreaView style={styles.container}>
+				<KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+						<View>
+							<Text style={styles.title}>Cadastro de produto</Text>
+							<ScrollView>
+								<TextInput
+									style={[styles.input, (isFocus1 || isFilled1) && { borderColor: '#5cd65c' }]}
+									placeholder='Nome do produto'
+									onFocus={handleFocus1}
+									onBlur={handleBlur1}
+									onChangeText={handleInputChange1}
+								></TextInput>
+								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name1Error}</Text>
+								<Text style={styles.inputHint}>Categoria do produto</Text>
+								<Picker
+									style={styles.select}
+									selectedValue={categoria}
+									onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}
+								>
+									<Picker.Item label='Fruta' value='fruta' />
+									<Picker.Item label='Grão' value='grao' />
+									<Picker.Item label='Verdura' value='verdura' />
+									<Picker.Item label='Legume' value='legume' />
+									<Picker.Item label='Hortaliça' value='hortaliça' />
+								</Picker>
+								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name2Error}</Text>
+								<TextInput
+									style={[styles.input, (isFocus2 || isFilled2) && { borderColor: '#5cd65c' }]}
+									placeholder='Caixas disponíveis'
+									onFocus={handleFocus3}
+									onBlur={handleBlur3}
+									onChangeText={handleInputChange3}
+									keyboardType='numeric'
+								></TextInput>
+								<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name3Error}</Text>
+								<DatePicker
+									style={styles.datePickerStyle}
+									date={date}
+									mode='date'
+									placeholder='Data de validade'
+									format='YYYY-MM-DD'
+									confirmBtnText='Confirm'
+									cancelBtnText='Cancel'
+									customStyles={{
+										dateIcon: {
+											position: 'absolute',
+											left: 0,
+											top: 4,
+											marginLeft: 0,
+										},
+										dateInput: {
+											marginLeft: 36,
+										},
+									}}
+									onDateChange={(date) => {
+										setDate(date);
+									}}
+								/>
+								<View style={{ alignItems: 'center', justifyContent: 'space-around' }}>
+									{/* <Button  title='Cadastrar' activeOpacity={0.7} onPress={handleSubmit} /> */}
 
-						<TextInput
-							style={[styles.input, (isFocus2 || isFilled2) && { borderColor: '#5cd65c' }]}
-							placeholder='Categoria do produto'
-							onFocus={handleFocus2}
-							onBlur={handleBlur2}
-							onChangeText={handleInputChange2}
-						></TextInput>
-						<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name2Error}</Text>
-						<TextInput
-							style={[styles.input, (isFocus2 || isFilled2) && { borderColor: '#5cd65c' }]}
-							placeholder='Caixas disponíveis'
-							onFocus={handleFocus3}
-							onBlur={handleBlur3}
-							onChangeText={handleInputChange3}
-							keyboardType='numeric'
-						></TextInput>
-						<Text style={{ color: 'tomato', alignSelf: 'center' }}>{name3Error}</Text>
-						<DatePicker
-							style={styles.datePickerStyle}
-							date={date}
-							mode='date'
-							placeholder='Data de validade'
-							format='YYYY-MM-DD'
-							confirmBtnText='Confirm'
-							cancelBtnText='Cancel'
-							customStyles={{
-								dateIcon: {
-									position: 'absolute',
-									left: 0,
-									top: 4,
-									marginLeft: 0,
-								},
-								dateInput: {
-									marginLeft: 36,
-								},
-							}}
-							onDateChange={(date) => {
-								setDate(date);
-							}}
-						/>
-						<View style={{ alignItems: 'center', justifyContent: 'space-around' }}>
-							<Button title='Cadastrar' activeOpacity={0.7} onPress={handleSubmit} />
-							<Text style={{ color: 'tomato', alignSelf: 'center' }}>{submitError}</Text>
-							<Button title='Listar produtos' onPress={goToListProduto} />
+									<Button
+										backgroundColor='#30B650'
+										label='Cadastrar'
+										labelStyle={{ fontWeight: '900' }}
+										style={{ marginBottom: 20, marginTop: 20 }}
+										iconSource={plusIcon}
+										enableShadow
+										borderRadius={10}
+										onPress={handleSubmit}
+									/>
+									<Text style={{ color: 'tomato', alignSelf: 'center' }}>{submitError}</Text>
+									{/* <Button title='Listar produtos' onPress={goToListProduto} /> */}
+								</View>
+							</ScrollView>
 						</View>
-					</View>
-				</TouchableWithoutFeedback>
-			</KeyboardAvoidingView>
-		</SafeAreaView>
+					</TouchableWithoutFeedback>
+				</KeyboardAvoidingView>
+			</SafeAreaView>
+		</ImageBackground>
 	);
 }
 
 const styles = StyleSheet.create({
 	title: {
-		fontSize: 25,
+		fontSize: 30,
 		lineHeight: 30,
 		textAlign: 'center',
+		marginTop: 80,
+		color: '#598234',
+		fontFamily: 'Roboto-Black',
+	},
+
+	image: {
+		flex: 1,
+		justifyContent: 'center',
 	},
 
 	container: {
@@ -205,13 +238,30 @@ const styles = StyleSheet.create({
 		marginTop: 50,
 		padding: 10,
 		borderBottomWidth: 1,
+		color: '#598234',
 		width: '100%',
 		textAlign: 'center',
+		fontFamily: 'Roboto-Black',
 	},
 
 	link: {
 		marginTop: 20,
 		color: '#737373',
+	},
+
+	inputHint: {
+		fontSize: 18,
+		marginTop: 10,
+		padding: 10,
+		color: 'black',
+		width: '100%',
+		textAlign: 'center',
+	},
+
+	select: {
+		fontSize: 15,
+		color: '#598234',
+		width: '100%',
 	},
 
 	datePickerStyle: {
